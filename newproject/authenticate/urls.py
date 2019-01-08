bffa92202e832f3632f9b4f0a682deacbc72cbca
petermakss <petermakss42@gmail.com>
2019-01-08 14:33:24 -0500

    Added authentication functionalities

--- newproject/authenticate/urls.py
+++ newproject/authenticate/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
    
     url('login/', views.login_user, name="login"),
     url('', views.home, name="home"),
+    url('register/', views.register_user, name='register'),
+    url('landing/', views.landing, name='landing'),
+    url('logout/', views.logout_user, name='logout'),
 
- ]
+ ] 
--- newproject/authenticate/views.py
+++ newproject/authenticate/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout 
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
 	return render(request, 'authenticate/home.html', {})
 
 def login_user(request):
-	return render(request, 'authenticate/login.html', {})
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, ('You Have Been Logged In!'))
+			return redirect('home')
+		else:
+			messages.success(request, ('Error Logging In. Please Try Again!'))
+			return redirect('login')
+
+	else:
+		return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, ('About to logout'))
+	return redirect('home')
+
+
+def register_user(request):
+	return render(request, 'authenticate/register.html', {})
+def landing(request):
+	return redirect(request, 'authenticate/landing.html', {})
