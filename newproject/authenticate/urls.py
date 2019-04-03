1e970c702f515fa071a4cb082fd60449ac58964d
petermakss <petermakss42@gmail.com>
2019-04-03 11:14:07 -0400

    complete authentican and profile setup

--- newproject/authenticate/urls.py
+++ newproject/authenticate/urls.py
@@ -3,12 +3,15 @@ from . import views
 
 
 urlpatterns = [
-   
+
     path('login/', views.login_user, name="login"),
     path('', views.home, name="home"),
     path('register/', views.register_user, name='register'),
     path('landing/', views.landing, name='landing'),
     path('logout/', views.logout_user, name='logout'),
+    path('viewprof/', views.view_prof, name='viewprof'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password',views.change_password, name='change_password'),
 
 
- ] 
+ ]
--- newproject/authenticate/views.py
+++ newproject/authenticate/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib import messages
+from .forms import SignUpForm, EditProfileForm
 
 # Create your views here.
 
@@ -17,7 +19,7 @@ def login_user(request):
 			messages.success(request, ('You Have Been Logged In!'))
 			return redirect('home')
 		else:
-			messages.success(request, ('Error Logging In. Please Try Again!'))
+			messages.success(request, ('Username or Password Wrong. Please Try Again!'))
 			return redirect('login')
 
 	else:
@@ -25,11 +27,51 @@ def login_user(request):
 
 
 def register_user(request):
-	return render(request, 'authenticate/register.html', {})
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username,password=password)
+			login(request,user)
+			messages.success(request, ('Registration Successful!'))
+			return redirect('home')
+	else:
+		form = SignUpForm()
+	context = {'form':form}
+	return render(request, 'authenticate/register.html', context)
 def landing(request):
 	return redirect(request, 'authenticate/landing.html', {})
+def view_prof(request):
+	return render(request, 'authenticate/viewprof.html', {})
 
 def logout_user(request):
 	logout(request)
 	messages.success(request, 'You have been logged out')
 	return redirect('home')
+
+def edit_profile(request):
+	if request.method == 'POST':
+		form = EditProfileForm(request.POST, instance=request.user)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('You Have Edited Your Profile.'))
+			return redirect('home')
+	else:
+		form = EditProfileForm(instance=request.user)
+	context = {'form':form}
+	return render(request, 'authenticate/edit_profile.html', context)
+
+def change_password(request):
+	if request.method == 'POST':
+		form = PasswordChangeForm(data=request.POST, user=request.user)
+		if form.is_valid():
+			form.save()
+			update_session_auth_hash(request, form.user)
+			messages.success(request, ('You Have Changed Your Password.'))
+			return redirect('home')
+	else:
+		form = PasswordChangeForm(user=request.user)
+	context = {'form':form}
+	return render(request, 'authenticate/change_password.html', context)
